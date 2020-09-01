c10b778a9a3091450df2a2ed2ad0a962b390c7c0
Gurleen Singh <gurleen@wewager.io>
2020-09-01 11:29:13 -0400

    Added Product, ProductImage, CartItem

--- store/models.py
+++ store/models.py
@@ -1,2 +1,19 @@
 from django.db import models
 
+
+class Product(models.Model):
+    name = models.TextField(max_length=50)
+    brand = models.TextField(max_length=30)
+    description = models.TextField(max_length=500)
+    quantity = models.IntegerField()
+    price = models.IntegerField()
+
+
+class ProductImage(models.Model):
+    product = models.ForeignField(Product, default=None)
+    image = models.ImageField(upload_to="img/")
+
+
+class CartItem(models.Model):
+    product = models.ForeignField(Product, default=None)
+    amount = models.IntegerField()
