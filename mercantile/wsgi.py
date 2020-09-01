b4016f33cebf586be344e6232b174a861d6a7599
Gurleen Singh <gurleen@wewager.io>
2020-09-01 11:18:11 -0400

    Initial commit

---
+++ mercantile/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for mercantile project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mercantile.settings')
+
+application = get_wsgi_application()
