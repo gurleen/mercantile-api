3c9fd8a0332b78c59cfadf5dbf3df8b896b0aeff
Gurleen Singh <gurleen@wewager.io>
2020-09-01 17:05:42 -0400

    Added address to order model

--- store/models.py
+++ store/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from address.models import AddressField
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
@@ -22,4 +23,5 @@ class CartItem(models.Model):
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    address = AddressField()
     items = models.ManyToManyField(CartItem)
