913b2fb4204c250d937de3066d98c562bb70a4d9
Gurleen Singh <gurleen@wewager.io>
2020-09-01 16:40:07 -0400

    Added Address and Order models

--- store/models.py
+++ store/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Product(models.Model):
-    name = models.TextField(max_length=50)
-    brand = models.TextField(max_length=30)
-    description = models.TextField(max_length=500)
+    name = models.CharField(max_length=50)
+    brand = models.CharField(max_length=30)
+    description = models.TextField()
     quantity = models.IntegerField()
     price = models.IntegerField()
 
@@ -15,5 +16,18 @@ class ProductImage(models.Model):
 
 
 class CartItem(models.Model):
-    product = models.ForeignField(Product, default=None)
+    product = models.ForeignKey(Product, default=None)
     amount = models.IntegerField()
+
+
+class Address(models.Model):
+    house_num = models.CharField(max_length=5)
+    street = models.CharField(max_length=40)
+    city = models.CharField(max_length=20)
+    state = models.CharField(max_length=4)
+    phone = models.CharField()
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, default=None)
+    items = models.ManyToManyField(CartItem)
