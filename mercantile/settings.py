e1d4015cde1dafb63befa43f1dab5d00aa38efe6
Gurleen Singh <gurleen@wewager.io>
2020-09-01 21:04:12 -0400

    Added django-storages, boto3 and S3 config

--- mercantile/settings.py
+++ mercantile/settings.py
@@ -106,3 +106,13 @@ STATIC_URL = "/static/"
 
 # django-address
 GOOGLE_API_KEY = env("GOOGLE_API_KEY")
+
+# django-storages
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME")
+AWS_S3_ENDPOINT_URL = env("AWS_S3_ENDPOINT_URL")
